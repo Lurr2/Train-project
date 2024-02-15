1b75d6f09c12e01f154931ae89b32e823ed50b42
Jakub Kasperski <jkasperski1995@gmail.com>
2024-02-15 10:37:18 +0100

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenubarComponent } from './components/menubar/menubar.component';
@@ -9,6 +9,12 @@ import { InspirationComponent } from './components/common/inspiration/inspiratio
 import { NewsComponent } from './components/common/news/news.component';
 import { ContactComponent } from './components/common/contact/contact.component';
 
+const routes: Routes = [
+  {path: 'promotion', component: PromotionComponent},
+  {path: 'inspiration', component: InspirationComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'contact', component: ContactComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,8 +26,10 @@ import { ContactComponent } from './components/common/contact/contact.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
