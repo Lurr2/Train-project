aa70a6d2cadcc7f3167a85b5dca1d5e4d39f04f2
Jakub Kasperski <jkasperski1995@gmail.com>
2024-02-15 10:16:38 +0100

    creating components for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenubarComponent } from './components/menubar/menubar.component';
+import { PromotionComponent } from './components/common/promotion/promotion.component';
+import { InspirationComponent } from './components/common/inspiration/inspiration.component';
+import { NewsComponent } from './components/common/news/news.component';
+import { ContactComponent } from './components/common/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenubarComponent
+    MenubarComponent,
+    PromotionComponent,
+    InspirationComponent,
+    NewsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
