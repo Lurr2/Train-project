f6ebcfafb9531330c06786df38d448bb3713df80
Jakub Kasperski <jkasperski1995@gmail.com>
2024-02-15 13:41:02 +0100

    adding buttons for translation

--- src/app/components/menubar/menubar.component.ts
+++ src/app/components/menubar/menubar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenubarComponent implements OnInit {
 
-  constructor() {
-   
+  constructor(public translate: TranslateService) {  }
+  switchLanguage(lang: string){
+    this.translate.use(lang);
   }
-
   ngOnInit(): void {
   }
 
