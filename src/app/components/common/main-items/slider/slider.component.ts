187203e6d41a1a103dda00b8527932d79b4e1ece
Jakub Kasperski <jkasperski1995@gmail.com>
2024-02-16 14:37:30 +0100

    added test slider

--- src/app/components/common/main-items/slider/slider.component.ts
+++ src/app/components/common/main-items/slider/slider.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
+  images: { url: string, caption: string }[] = [
+    { url: 'https://via.placeholder.com/600x400', caption: 'First slide' },
+    { url: 'https://via.placeholder.com/600x400', caption: 'Second slide' },
+    { url: 'https://via.placeholder.com/600x400', caption: 'Third slide' }
+  ];
+  
+  currentIndex: number = 0;
+  
+  
 
+  showNext() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+  showPrev() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+  
+  
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
