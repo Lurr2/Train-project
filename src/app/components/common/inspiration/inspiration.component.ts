fe142617541a9409bb912a4fcb026120d88f93bf
Jakub Kasperski <jkasperski1995@gmail.com>
2024-02-29 15:34:26 +0100

    added beta popup

--- src/app/components/common/inspiration/inspiration.component.ts
+++ src/app/components/common/inspiration/inspiration.component.ts
@@ -7,14 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inspiration.component.scss'],
 })
 export class InspirationComponent implements OnInit {
+  isPopupOpen: boolean = false;
   inspirationData: any;
 
   constructor(private productsService: ProductsService) {}
+  togglePopup() {
+    this.isPopupOpen = !this.isPopupOpen;
+  }
 
   ngOnInit(): void {
     this.productsService.getProducts('inspiration').subscribe((data) => {
       this.inspirationData = data;
-      console.log('inspirationData');
     });
   }
 }
